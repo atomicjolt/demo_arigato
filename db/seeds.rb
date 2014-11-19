8cf1216b0411e0c05d29bae1267bbcf1fbb242d2
Justin Ball <justinball@gmail.com>
2014-11-19 14:57:54 -0700

    Change default Canvas domains

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ if Rails.env.production?
     domain: 'http://demoarigato.herokuapp.com',
     lti_key: 'demoarigato',
     lti_secret: '81bb01a80454b7b5a587cfbf1e745d501c0f3b2e3e36b0e60240a2fe7ba8daac0b588678ca8dfaf0d4341017e6646d03751df63c4056a85e01a22e3ce72a8e29',
-    canvas_uri: 'https://canvas.instructure.com'
+    canvas_uri: 'https://demo.instructure.com'
   }]
 else
   accounts = [{
@@ -18,7 +18,7 @@ else
     domain: 'http://arigator.ngrok.com',
     lti_key: 'demoarigato',
     lti_secret: 'd52ca2c9892975bbb9def56e68eefe8e92a338d9b74d73ec5dad64803a376b2f1f5129c0bd9f7e73684526c234e0835bd1635e09d427cd45cb0de4296278682f',
-    canvas_uri: 'https://canvas.instructure.com'
+    canvas_uri: 'https://atomicjolt.instructure.com'
   }]
 end
 
