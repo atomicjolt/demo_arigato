84cf42dd0dd050d0c2dd81361e587fd73907b227
Justin Ball <justinball@gmail.com>
2014-11-21 09:27:03 -0700

    Fixed factory

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -1,13 +1,14 @@
+course_content = {
+  course_code: "k12-algebra",
+  name: "Algebra 1A",
+  sis_course_id: 107740,
+  status: "active",
+  catridge: "HE-K12-algebra-master-export.imscc"
+}
+
 FactoryGirl.define do
   factory :course do
     canvas_load
-    content: {
-        course_code: "k12-algebra",
-        name: "Algebra 1A",
-        sis_course_id: 107740,
-        status: "active",
-        catridge: "HE-K12-algebra-master-export.imscc"
-      }
+    content course_content
   end
-
 end
